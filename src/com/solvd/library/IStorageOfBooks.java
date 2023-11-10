c5aafe2bb09f516ccceb3982ba6db1a4ee3a28d8
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2023-11-10 21:10:05 +0100

    Delete src/com/solvd/library/IStorageOfBooks.java
    delete IStorageOfBooks.java

--- src/com/solvd/library/IStorageOfBooks.java
+++
@@ -1,6 +0,0 @@
-package com.solvd.library;
-
-public interface IStorageOfBooks {
-    List<Book> searchForBooksByTitle(String searchedTitle);
-    List<Book> searchForBooksByGenre(LiteratureGenre searchedGenre)
-}
