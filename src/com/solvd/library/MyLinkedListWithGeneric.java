c4869f67e32cf8cd72e9b9f9d75cdb4d938b013f
PiotrPor <piotr.porembinski@outlook.com>
2023-11-14 20:55:04 +0100

    LinkedList with genericdone correctly
    Class MyLinkedListWithGeneric is remade/rewritten to be now defined/made correctly.
    Now it doesn't use/implement the default LinkedList.

--- src/com/solvd/library/MyLinkedListWithGeneric.java
+++ src/com/solvd/library/MyLinkedListWithGeneric.java
@@ -6,37 +6,90 @@ import java.util.Collection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//first of 2 classes in this file
+//this is a helper class
+class Node<T> {
+    T dataStoredInHere;
+    Node<T> nextNode;
+
+    Node(T givenData) {
+        this.dataStoredInHere = givenData;
+        this.nextNode = null;
+    }
+}
+
+//second of 2 classes in this file
+//this is the class we focus on
 public class MyLinkedListWithGeneric<T> {
-    public LinkedList<T> c;
+    private Node<T> headOfList;
+    private int length;
     private static final Logger LOGGER = LogManager.getLogger(MyLinkedListWithGeneric.class);
 
     public MyLinkedListWithGeneric() {
-        c = new LinkedList<>();
-    }
-
-    public MyLinkedListWithGeneric(Collection givenCollection) {
-        c = new LinkedList<>(givenCollection);
+        headOfList = null;
+        length = 0;
     }
 
     public void add(T something) {
-        c.add(something);
+        Node<T> nodeToBeAdded = new Node<>(something);
+        Node<T> temporary = headOfList;
+        if (temporary == null) {
+            headOfList = nodeToBeAdded;
+        } else {
+            temporary = temporary.nextNode;
+            while (temporary.nextNode != null) {
+                temporary = temporary.nextNode;
+            }
+            temporary.nextNode = nodeToBeAdded;
+        }
+        length += 1;
     }
 
     public T get(int index) {
-        T toBeReturned = null;
-        try {
-            toBeReturned = c.get(index);
-        } catch(IndexOutOfBoundsException e) {
-            LOGGER.warn("Exception when accessing LinkedList: "+ e.getMessage());
+        Node<T> currentNode = headOfList;
+        for (int i = 0; i < index && currentNode != null; i++) {
+            currentNode = currentNode.nextNode;
         }
-        return toBeReturned;
+        if (currentNode != null) {
+            return currentNode.dataStoredInHere;
+        } else {
+            return null;
+        }
+    }
+
+    public void remove(T elementToDelete) {
+        if (headOfList == null) {
+            return;
+        }
+
+        if (headOfList.dataStoredInHere.equals(elementToDelete)) {
+            headOfList = headOfList.nextNode;
+            return;
+        }
+
+        Node<T> curr = headOfList;
+        while (!(curr.nextNode == null || curr.nextNode.dataStoredInHere.equals(elementToDelete))) {
+            curr = curr.nextNode;
+        }
+
+        if (curr.nextNode != null) {
+            curr.nextNode = curr.nextNode.nextNode;
+        }
+    }
+
+    public void remove(int i) {
+        remove(this.get(i));
     }
 
     public boolean isEmpty() {
-        return c.isEmpty();
+        if (length == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int size() {
-        return c.size();
+        return length;
     }
 }
